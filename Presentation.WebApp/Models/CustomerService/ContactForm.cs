1e0c32be70ac874b39ecc3f3904258e1abc1c1b9
Sharbel Kas Elias <sharbel2001@live.com>
2026-04-18 12:50:09 +0200

    Add customer service page with contact form, FAQ section and their styling

---
+++ Presentation.WebApp/Models/CustomerService/ContactForm.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.WebApp.Models.CustomerService;
+
+public class ContactForm
+{
+    [Required(ErrorMessage = "First name is required")]
+    [DataType(DataType.Text)]
+    [Display(Name = "First Name *", Prompt = "Enter First Name")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last name is required")]
+    [DataType(DataType.Text)]
+    [Display(Name = "Last Name *", Prompt = "Enter Last Name")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email address is required")]
+    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Email address must be valid")]
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email Address *", Prompt = "Enter Email Address")]
+    public string Email { get; set; } = null!;
+
+    [Phone(ErrorMessage = "Phone number must be valid")]
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "Phone Number", Prompt = "Enter Phone Number")]
+    public string? Phone {  get; set; }
+
+    [Required(ErrorMessage = "Message is required")]
+    [DataType(DataType.MultilineText)]
+    [Display(Name = "Message *", Prompt = "Message...")]
+    public string Message { get; set; } = null!;
+}
